5a461d24da3d73ad121ab7b0516c7b9e053c0072
Duyky <Duykyxk4797@gmail.com>
2020-08-17 16:25:28 +0700

    first commit

---
+++ E-commerce-Complate-Flutter-UI-master/lib/routs.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+import 'package:shop_app/screens/login/login_screen.dart';
+import 'package:shop_app/screens/splash/splash_screen.dart';
+
+// We use name route
+// All our routes will be available here
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+  LoginScreen.routeName: (context) => LoginScreen(),
+};
